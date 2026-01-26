8a1307caa3f2a885267e96ad6d20fe2a7d7bcada
pharmmiracle123 <onyekamiracle08@gmail.com>
2026-01-26 14:02:18 +0100

    Create db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+if ($conn->connect_error) {
+    die("Database connection failed");
+}
+?>
